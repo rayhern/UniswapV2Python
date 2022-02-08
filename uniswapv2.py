e744c8a6bf49f0d698a01900b0ba919991edaed0
rayhern <bizong@gmail.com>
2022-02-08 14:49:10 -0800

    chownt.

--- uniswapv2.py
+++ uniswapv2.py
@@ -444,15 +444,15 @@ class UniswapV2():
         
         token0_pool_amount = Decimal(pair_balance / (total_supply / reserves[0]))
         token1_pool_amount = Decimal(pair_balance / (total_supply / reserves[1]))
-        
-        token1_pool_amount = Web3.fromWei(token1_pool_amount, "ether")
 
         if str(token0) != str(value_token):
+            token1_pool_amount = Web3.fromWei(token1_pool_amount, "ether")
             token0_value = Web3.fromWei(self._get_amounts_out(1, [token0, value_token])[1], "ether") * token0_pool_amount
         else:
-            token0_value = Web3.fromWei(token0_pool_amount, "ether")
+            token0_value = token0_pool_amount
         
         if str(token1) != str(value_token):
+            token0_pool_amount = Web3.fromWei(token0_pool_amount, "ether")
             token1_value = Web3.fromWei(self._get_amounts_out(1, [token1, value_token])[1], "ether") * token1_pool_amount
         else:
             token1_value = token1_pool_amount
